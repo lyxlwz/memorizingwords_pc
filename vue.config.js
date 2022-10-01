eb2b3bb5329c28a87bb60f809dd3f317dcbc8f12
lyxlwz <1850115720@qq.com>
2022-10-01 16:09:55 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 }
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/memorizingwords_pc' : '/',
-  outputDir: 'dist',
+  outputDir: 'memorizingwords_pc',
   assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
